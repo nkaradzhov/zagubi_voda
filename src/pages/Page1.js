d8a2a90778cbf5deffb93a9341864606638e4016
hukuuu <hukuuu@gmail.com>
2018-05-26 09:33:22 +0300

    make page1 functional

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
-import { connect } from 'react-redux'
 import NumberInput from '../components/NumberInput'
+import { connect } from 'react-redux'
 
 import {
   updateVlezen,
@@ -51,62 +51,56 @@ const Tr = ({
   </tr>
 )
 
-class Page1 extends Component {
-  render() {
-    const {
-      data,
-      minRow,
-      setVlezen,
-      setSreden,
-      setKritichen,
-      setProtok
-    } = this.props
-
-    return (
-      <Grid>
-        <Row className="show-grid">
-          <Col lg={8} sm={10}>
-            <table>
-              <thead>
-                <tr>
-                  <th>Час</th>
-                  <th>Влезен притисок (m)</th>
-                  <th>Среден притисок (m)</th>
-                  <th>Критичен притисок (m)</th>
-                  <th>Влезен проток (m3/h)</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.keys(data).map(key => (
-                  <Tr
-                    obj={data[key]}
-                    key={key}
-                    onVlezenChange={setVlezen(key)}
-                    onSredenChange={setSreden(key)}
-                    onKritichenChange={setKritichen(key)}
-                    onProtokChange={setProtok(key)}
-                  />
-                ))}
-                <tr>
-                  <td colSpan="4">
-                    Минималната ноќна потрошувачка (измерена), m3/h
-                  </td>
-                  <td>{minRow ? minRow.protok : 'n/a'}</td>
-                </tr>
-                <tr>
-                  <td colSpan="4">
-                    Средна вредност на ноќниот притисок во мрежата
-                  </td>
-                  <td>{minRow ? minRow.sreden : 'n/a'}</td>
-                </tr>
-              </tbody>
-            </table>
-          </Col>
-        </Row>
-      </Grid>
-    )
-  }
-}
+const Page1 = ({
+  data,
+  minRow,
+  setVlezen,
+  setSreden,
+  setKritichen,
+  setProtok
+}) => (
+  <Grid>
+    <Row className="show-grid">
+      <Col lg={8} sm={10}>
+        <table>
+          <thead>
+            <tr>
+              <th>Час</th>
+              <th>Влезен притисок (m)</th>
+              <th>Среден притисок (m)</th>
+              <th>Критичен притисок (m)</th>
+              <th>Влезен проток (m3/h)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data).map(key => (
+              <Tr
+                obj={data[key]}
+                key={key}
+                onVlezenChange={setVlezen(key)}
+                onSredenChange={setSreden(key)}
+                onKritichenChange={setKritichen(key)}
+                onProtokChange={setProtok(key)}
+              />
+            ))}
+            <tr>
+              <td colSpan="4">
+                Минималната ноќна потрошувачка (измерена), m3/h
+              </td>
+              <td>{minRow ? minRow.protok : 'n/a'}</td>
+            </tr>
+            <tr>
+              <td colSpan="4">
+                Средна вредност на ноќниот притисок во мрежата
+              </td>
+              <td>{minRow ? minRow.sreden : 'n/a'}</td>
+            </tr>
+          </tbody>
+        </table>
+      </Col>
+    </Row>
+  </Grid>
+)
 
 const mstp = state => ({
   data: state.page1,
