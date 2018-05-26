3ebd377312f0032a033c6bf64b8c49168e661b33
hukuuu <hukuuu@gmail.com>
2018-05-26 19:50:20 +0300

    fix numberinput

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+const re = /^-?\d+\.?\d*$/
+const couldBeNumber = string =>
+  string === '-' || string === '' || re.test(string)
+
 const NumberInput = ({ value, onChange, ...rest }) => (
   <input
     type="text"
     value={value}
     onChange={e => {
-      e.target.value === '-' && onChange(e.target.value)
-      const val = parseInt(e.target.value, 10)
-      !isNaN(val) && onChange(val)
+      console.log(e.target.value, couldBeNumber(e.target.value))
+      if (couldBeNumber(e.target.value)) {
+        onChange && onChange(e.target.value)
+      }
     }}
     {...rest}
   />
