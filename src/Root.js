01b9a51ed1975c1a84be2bbd22e00ae23ba7ef89
hukuuu <hukuuu@gmail.com>
2018-06-13 19:12:18 +0300

    enabling gh pages

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navigation from './components/Navigation'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
