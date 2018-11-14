4d78cf5aea2068625b5c842c8e4bdf97c83cf3b4
hukuuu <hukuuu@gmail.com>
2018-11-14 10:29:00 +0200

    trim -> round

--- src/util/round.js
+++ src/util/round.js
@@ -2,7 +2,7 @@ export default (value, digits = 4) =>
   value
     ? typeof value === 'number'
       ? (
-          parseInt(value * Math.pow(10, digits), 10) / Math.pow(10, digits)
+          Math.round(value * Math.pow(10, digits), 10) / Math.pow(10, digits)
         ).toFixed(digits)
       : value
     : ''
